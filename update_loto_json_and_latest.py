af9ba95413a6755f9468f36c66239e64394a7b6b
po-san <goolegleu@gmail.com>
2025-07-23 10:47:12 +0900

    auto update: all loto JSONs

--- update_loto_json_and_latest.py
+++ update_loto_json_and_latest.py
@@ -11,7 +11,7 @@ CONFIGS = [
     {
         "name": "ロト6",
         "url": "https://takarakuji.rakuten.co.jp/backnumber/loto6/",
-        "save_path": "./loto6-data/loto6.json",
+        "save_path": "/Users/po-san/hatena/loto6-data/loto6.json",
         "num_cnt": 6,
         "bonus_keys": ["ボーナス数字"],
         "carry_key": "キャリーオーバー",
@@ -21,7 +21,7 @@ CONFIGS = [
     {
         "name": "ミニロト",
         "url": "https://takarakuji.rakuten.co.jp/backnumber/mini/",
-        "save_path": "./miniloto-data/miniloto.json",
+        "save_path": "/Users/po-san/hatena/miniloto-data/miniloto.json",
         "num_cnt": 5,
         "bonus_keys": ["ボーナス数字"],
         "carry_key": None,
@@ -31,7 +31,7 @@ CONFIGS = [
     {
         "name": "ロト7",
         "url": "https://takarakuji.rakuten.co.jp/backnumber/loto7/",
-        "save_path": "./loto7-data/loto7.json",
+        "save_path": "/Users/po-san/hatena/loto7-data/loto7.json",
         "num_cnt": 7,
         "bonus_keys": ["BONUS数字1", "BONUS数字2"],
         "carry_key": "キャリーオーバー",
